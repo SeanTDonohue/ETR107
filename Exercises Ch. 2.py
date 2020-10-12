637346f5bcae512c11da7d58ba0ee8ca562db7f8
SeanTDonohue <70673362+SeanTDonohue@users.noreply.github.com>
2020-10-11 21:15:38 -0400

    Add files via upload

---
+++ Exercises Ch. 2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 14:40:05 2020
+
+@author: stimo
+"""
+import math
+#Exercise 2.1
+42 = n
+# SyntaxError: cannot assign to literal
+x = y = 1
+# this seems to be allowed
+# a semicolon at the end of a statement gives an invalid syntax error
+# I was also given a syntax error for a period
+# "XY name is not defined
+
+
+#Exercise 2.2
+4/3 pi 5^2
+24.95(.40)
+= 9.98
+24.95-9.98
+#cost per book with discount
+= 14.969
+#total book cost
+898.14
+#shipping costs
+47.25
+#total wholesale cost 
+945.39
+#Arrives home at 7:36
+7:36
+
+
+
